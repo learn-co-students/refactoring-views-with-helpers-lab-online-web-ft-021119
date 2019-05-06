ef1003d4209b037c6ef14d870322a384e2f75ca7
ChrstphGrnr <ChristophGruener@me.com>
2019-05-06 17:26:56 -0600

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,9 @@
 module ArtistsHelper
+    def display_artist(song)
+        if song.artist_id 
+            link_to Artist.find(song.artist_id).name, artist_path(song.artist_id)
+        else
+            link_to 'Add Artist', edit_song_path(song) 
+        end
+    end
 end
